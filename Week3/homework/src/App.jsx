496651777b8362896049a4f4d827b3c96fcf3762
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-07 17:12:47 +0900

    style: App.jsx에 글로벌 스타일 설정

--- Week3/homework/src/App.jsx
+++ Week3/homework/src/App.jsx
@@ -1,12 +1,21 @@
+import { Global, css } from '@emotion/react';
 import Header from "./components/Header";
 import { useState } from "react";
 
+const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   const [activeBtn, setActiveBtn] = useState('game');
 
   return (
     <>
+      <Global styles={globalStyles} />
       <Header activeBtn={activeBtn} onBtnChange={setActiveBtn} />
       {activeBtn === 'game' ? '게임페이지' : '랭킹페이지'}
     </>
