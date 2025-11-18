8b1a6ef112cab9f2772d9f39cfa2ffdf480a8d01
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-18 13:51:46 +0900

    chore: 중복 의존성 제거

--- Week4/homework/src/routes/index.tsx
+++ Week4/homework/src/routes/index.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "@/layouts";
 import LoginPage from "@/pages/Login/LoginPage";
+import NotFoundPage from "@/pages/NotFoundPage";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
+    errorElement: <NotFoundPage />,
 		element: <Layout />,
 		children: [
 			{
--- Week4/homework/vite.config.ts
+++ Week4/homework/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+		tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 이 부분 추가
+    },
+  },
+});
+
